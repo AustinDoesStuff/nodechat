7a41696bec06e9f81fbde5ce8c917d413c04dfdc
Austin Tanner <austin.tanner04@gmail.com>
2017-11-02 11:40:53 -0500

    configured socket.io

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '..', '/public');
 const port = process.env.PORT || 3000
@@ -7,10 +9,21 @@ const port = process.env.PORT || 3000
 var app = express();
 app.use(express.static(publicPath));
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 app.get('', (req, res) => {
     res.render('index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Running on ${port}`);
 });
